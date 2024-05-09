205bc6f430ce1ab5a63f135c41881e5eee522cad
AndreeaBartic <128624386+AndreeaBartic@users.noreply.github.com>
2024-05-09 13:58:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/taskpro">
+      <BrowserRouter basename="/TaskPro">
         <App />
       </BrowserRouter>
     </PersistGate>
