d327b74cdcf2473922ac8e1fd285918cc3cd72b8
AndreeaBartic <128624386+AndreeaBartic@users.noreply.github.com>
2024-05-09 11:05:09 +0000

    Deploying to gh-pages from @ AndreeaBartic/TaskPro@6b5a78f1becfc9e0e5136e637ecd2df4b159dff4 🚀

--- src/components/Forms/NewBoardAndEditBoard/BgIcon.jsx
+++
@@ -1,9 +0,0 @@
-const BgIcon = ({ name }) => {
-  return (
-    <div>
-      <img src={name} alt={name} width={28} height={28} />
-    </div>
-  );
-};
-
-export default BgIcon;
--- src/services/trimStr.js
+++
@@ -1,6 +0,0 @@
-export function trimTitleString(str, num) {
-  if (str.length > num) {
-    return str.slice(0, num) + '...';
-  }
-  return str;
-}
